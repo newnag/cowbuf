a33688dcb599571b733e538f4f2cb91a43b96816
newnag <48855584+newnag@users.noreply.github.com>
2019-12-24 12:00:01 +0700

    หน้า detail product
    หน้า detail product

--- js/app.js
+++ js/app.js
@@ -33,3 +33,22 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// สไลด์รูปแบบที่ 2
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    arrows:false,
+    autoplay:false
+  });
