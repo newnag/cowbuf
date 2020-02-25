67f5f3259e41235d213d26a15e1d9da9cf1a06e4
newnag <48855584+newnag@users.noreply.github.com>
2020-02-25 10:21:15 +0700

    25/2/2020

--- page_member/login.php
+++ page_member/login.php
@@ -1,65 +1,56 @@
+
 <div class="dialog-privacy">
-    <div class="box-privacy">
-        <div class="box-text">
-            <p>
-            ยินดีต้อนรับสู่ www. วัวงามควายงาม.com ก่อตั้งเพื่อเป็นสื่อกลาง แลกเปลี่ยน แหล่งความรู้ และพัฒนาอาชีพเกษตรกรรมให้เกิดความมั่นคง ไม่จำกัดเพศ  อายุ วุฒิการศึกษา อาชีพ หรือประสบการณ์ มีคำแนะนำสำหรับสมาชิกดังนี้
-            </p>
-            <p>1) ชื่อที่ใช้ควรเป็นชื่อที่เป็นมงคล บ่งบอกถึงความเจริญก้าวหน้า ไม่สั้นหรือยาวเกินไป</p>
-            <p>2) ข้อมูลที่ใช้เป็นข้อเท็จจริง</p>
-            <p>3) มีเจตนาที่จะใช้ www.วัวงามควายงาม.com พัฒนาตนเอง ขยายกิจการ เผยแพร่ผลงานและแบ่งปันแก่สมาชิก ชุมชนและสังคมให้เกิดความเจริญก้าวหน้า</p>
-            <p>4) ไม่แสดงเจตนา โพสต์ ข้อความ รูปภาพ ที่ผิดกฎหมาย สร้างความแตกแยก และขัดกับศีลธรรมอันดี</p>
-            <p>5) สมาชิกต้องเข้าใจและตระหนักถึงบทบาท หน้าที่ ความรับผิดชอบ ต่อสาธารณะ </p>
-            <p>6) ข้อความรูปภาพ การกระทำใดๆ ที่ www.วัวงามควายงาม.com ตรวจสอบแล้วมีลักษณะผิดกฏหมาย หรือมีลักษณะ ก่อกวน ยุยง ส่งเสริม ให้ร้าย ต่อบุคคลที่สาม หรือขัดต่อข้อตกลงและเงือนไขการให้บริการ สามารถลบข้อมูลนั้นได้ทั้งสิ้นโดยไม่จำเป็นต้องบอกกล่าวหรือแจ้งให้ทราบ</p>
-        </div>
-
-        <div class="check-confirm">
-            <input type="checkbox" id="check-confirm">
-            <span>ข้าพเจ้ายินยอมเงื่อนไขและข้อกำหนด</span>
-        </div>
-
-        <div class="confirm-ok">
-            <button>รับทราบข้อกำหนด</button>
-        </div>
+  <div class="box-privacy">
+    <div class="box-text">
+      <p>
+        ยินดีต้อนรับสู่ www. วัวงามควายงาม.com ก่อตั้งเพื่อเป็นสื่อกลาง แลกเปลี่ยน แหล่งความรู้ และพัฒนาอาชีพเกษตรกรรมให้เกิดความมั่นคง ไม่จำกัดเพศ อายุ วุฒิการศึกษา อาชีพ หรือประสบการณ์ มีคำแนะนำสำหรับสมาชิกดังนี้
+      </p>
+      <p>1) ชื่อที่ใช้ควรเป็นชื่อที่เป็นมงคล บ่งบอกถึงความเจริญก้าวหน้า ไม่สั้นหรือยาวเกินไป</p>
+      <p>2) ข้อมูลที่ใช้เป็นข้อเท็จจริง</p>
+      <p>3) มีเจตนาที่จะใช้ www.วัวงามควายงาม.com พัฒนาตนเอง ขยายกิจการ เผยแพร่ผลงานและแบ่งปันแก่สมาชิก ชุมชนและสังคมให้เกิดความเจริญก้าวหน้า</p>
+      <p>4) ไม่แสดงเจตนา โพสต์ ข้อความ รูปภาพ ที่ผิดกฎหมาย สร้างความแตกแยก และขัดกับศีลธรรมอันดี</p>
+      <p>5) สมาชิกต้องเข้าใจและตระหนักถึงบทบาท หน้าที่ ความรับผิดชอบ ต่อสาธารณะ </p>
+      <p>6) ข้อความรูปภาพ การกระทำใดๆ ที่ www.วัวงามควายงาม.com ตรวจสอบแล้วมีลักษณะผิดกฏหมาย หรือมีลักษณะ ก่อกวน ยุยง ส่งเสริม ให้ร้าย ต่อบุคคลที่สาม หรือขัดต่อข้อตกลงและเงือนไขการให้บริการ สามารถลบข้อมูลนั้นได้ทั้งสิ้นโดยไม่จำเป็นต้องบอกกล่าวหรือแจ้งให้ทราบ</p>
     </div>
+    <div class="check-confirm">
+      <input type="checkbox" id="check-confirm" name="check-confirm">
+      <label for="check-confirm">ข้าพเจ้ายินยอมเงื่อนไขและข้อกำหนด</label>
+    </div>
+    <div class="confirm-ok">
+      <button>รับทราบข้อกำหนด</button>
+    </div>
+  </div>
 </div>
 
-<div class="login">
-    <div class="head-text">
-        <div class="text">
-            <h1>เข้าสู่ระบบ</h1>
+<div class="login" id="formLogin">
+  <div class="head-text">
+    <div class="text">
+      <h1><?=$cate_login['cate_name']?></h1>
+    </div>
+  </div>
+  <div class="form-login">
+    <h2>กรอกข้อมูลเพื่อเข้าสู่ระบบ</h2>
+    <form action="">
+      <div class="input-login">
+        <label>ชื่อผู้ใช้*</label>
+        <input name="user" type="text" id="login_username" placeholder="กรอกชื่อผู้ใช้" maxlength="32">
+      </div>
+      <div class="input-login">
+        <label>รหัสผ่าน*</label>
+        <input name="pass" id="pass" type="password" placeholder="กรอกรหัสผ่าน" maxlength="32">
+        <div class="view" onclick="view_password()"><i class="fas fa-eye"></i></div></input>
+      </div>
+      <div class="forget-register">
+        <div class="forget">
+          <a href="<?=SITE_URL.$cate_forgetPassword['url']?>"><?=$cate_forgetPassword['cate_name']?></a>
         </div>
-    </div>
-
-    <div class="form-login">
-        <h2>กรอกข้อมูลเพื่อเข้าสู่ระบบ</h2>
-
-        <form action="">
-            <div class="input-login">
-                <label>ชื่อผู้ใช้*</label>
-                <input name="user" type="text" placeholder="กรอกชื่อผู้ใช้">
-            </div>
-
-            <div class="input-login">
-                <label>รหัสผ่าน*</label>
-                <input name="pass" id="pass" type="password" placeholder="กรอกรหัสผ่าน">
-                <div class="view" onclick="view_password()"><i class="fas fa-eye"></i></div></input>
-            </div>
-        
-
-            <div class="forget-register">
-                <div class="forget">
-                    <a href="forget">ลืมรหัสผ่าน</a>
-                </div>
-
-                <div class="register">
-                    <a href="register">สมัครสมาชิก</a>
-                </div>
-            </div>
-
-            <div class="login-button">
-                <button>เข้าสู่ระบบ</button>
-            </div>
-
-        </form>
-    </div>
+        <div class="register">
+          <a href="<?=SITE_URL.$cate_register['url']?>"><?=$cate_register['cate_name']?></a>
+        </div>
+      </div>
+      <div class="login-button">
+        <button>เข้าสู่ระบบ</button>
+      </div>
+    </form>
+  </div>
 </div>
--- page_member/register.php
+++ page_member/register.php
@@ -1,66 +1,61 @@
 <div class="register-page">
-    <div class="head-text">
-        <div class="text">
-            <h1>สมัครสมาชิก</h1>
-        </div>
+  <div class="head-text">
+    <div class="text">
+      <h1>สมัครสมาชิก</h1>
     </div>
-
-    <div class="form-register">
-        <h2>กรอกข้อมูลเพื่อเข้าสู่ระบบ</h2>
-
-        <form action="">
-            <div class="input-login">
-                <label>ชื่อผู้ใช้*</label>
-                <input type="text" placeholder="กรอกชื่อผู้ใช้">
-            </div>
-
-            <div class="input-login">
-                <label>รหัสผ่าน*</label>
-                <input id="pass" type="password" placeholder="กรอกรหัสผ่าน">
-                <div class="view" onclick="view_password()"><i class="fas fa-eye"></i></div></input>
-            </div>
-
-            <div class="input-login">
-                <label>ยืนยันรหัสผ่าน*</label>
-                <input id="confpass" type="text" placeholder="กรอกยืนยันรหัสผ่าน">
-            </div>
-
-            <div class="input-login">
-                <label>ชื่อสกุล*</label>
-                <input type="text" placeholder="กรอกชื่อ-สกุล">
-            </div>
-
-            <div class="input-login">
-                <label>เลขบัตรประชาชน*</label>
-                <input type="number" placeholder="กรอกเลขบัตรประชาชน">
-            </div>
-
-            <div class="input-login">
-                <label>จังหวัด*</label>
-                <select name="" id="">
-                    <option value="">เลือกจังหวัด</option>
-                </select>
-            </div>
-
-            <div class="input-login">
-                <label>เบอร์โทร*</label>
-                <input type="tel" placeholder="กรอกเบอร์โทรศัพท์">
-            </div>
-
-            <div class="input-login">
-                <label>ผู้แนะนำ</label>
-                <input type="text" placeholder="กรอกชื่อผู้แนะนำ">
-            </div>
-
-            <div class="login-page">
-                <a href="login">เข้าสู่ระบบ</a>
-            </div>
-
-            <div class="login-button">
-                <button>ยืนยันการสมัคร</button>
-            </div>
-
-        </form>
-
-    </div>
+  </div>
+  <div class="form-register">
+    <h2>กรอกข้อมูลเพื่อเข้าสู่ระบบ</h2>
+    <form action="">
+      <div class="input-login">
+        <label>ชื่อผู้ใช้*</label>
+        <input type="text" id="register_username" placeholder="กรอกชื่อผู้ใช้">
+      </div>
+      <div class="input-login">
+        <label>รหัสผ่าน*</label>
+        <input id="pass" type="password" placeholder="กรอกรหัสผ่าน">
+        <div class="view" onclick="view_password()"><i class="fas fa-eye"></i></div></input>
+      </div>
+      <div class="input-login">
+        <label>ยืนยันรหัสผ่าน*</label>
+        <input id="confpass" type="password" placeholder="กรอกยืนยันรหัสผ่าน">
+      </div>
+      <div class="input-login">
+        <label>ชื่อสกุล*</label>
+        <input type="text" id="register_name" placeholder="กรอกชื่อ-สกุล">
+      </div>
+      <div class="input-login">
+        <label>เลขบัตรประชาชน*</label>
+        <input type="number" id="register_identification" placeholder="กรอกเลขบัตรประชาชน" max="13">
+      </div>
+      <div class="input-login">
+        <label>จังหวัด*</label>
+        <select name="" id="register_province">
+          <option value="">เลือกจังหวัด</option>
+          <?php
+          foreach ($Helper->getProvince() as $key => $res) {
+            echo '<option value="' . $res['id'] . '">' . $res['province_name'] . '</option>';
+          }
+          ?>
+        </select>
+      </div>
+      <div class="input-login">
+        <label>เบอร์โทร*</label>
+        <input type="tel" id="register_phone" placeholder="กรอกเบอร์โทรศัพท์">
+      </div>
+      <div class="input-login">
+        <label>ผู้แนะนำ</label>
+        <input type="text" id="register_adviser" placeholder="กรอกชื่อผู้แนะนำ">
+      </div>
+      <div class="login-page">
+        <a href="<?=SITE_URL.$urllogin['url']?>">เข้าสู่ระบบ</a>
+      </div>
+      <!-- <div class="login-page">
+        <a href="#" id="btn-register_test">กรอกข้อมูลทดสอบ</a>
+      </div> -->
+      <div class="login-button">
+        <button>ยืนยันการสมัคร</button>
+      </div>
+    </form>
+  </div>
 </div>
